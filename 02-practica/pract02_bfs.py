b3d48a391aa811814d00aaf5ee01d4b42d8bfe37
Elizabeth Mtz <lo.eliza@hotmail.com>
2023-10-09 20:14:53 -0600

    feat:prac2_inicio

---
+++ 02-practica/pract02_bfs.py
@@ -0,0 +1,135 @@
+import pygame
+import sys
+
+# Inicializar Pygame
+pygame.init()
+
+# Definir colores
+NEGRO = (0, 0, 0)
+BLANCO = (255, 255, 255)
+GRIS = (128, 128, 128)
+VERDE = (0, 255, 0)
+ROJO= (255, 0, 0)
+
+# Tamaño de la ventana y de la matriz
+ANCHO = 600
+ALTO = 600
+FILA = 15
+COLUMNA = 15
+
+# Tamaño de cada celda
+ANCHO_CELDA = ANCHO // COLUMNA
+ALTO_CELDA = ALTO // FILA
+
+# Definir la nueva matriz
+matriz = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1 ,0 ,1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1]
+]
+
+# Inicializar Pygame
+ventana = pygame.display.set_mode((ANCHO, ALTO))
+pygame.display.set_caption("Nueva Matriz 15x15")
+
+# Variables para el punto inicial
+punto_inicial_seleccionado = False
+punto_inicial = None
+
+# Variables para el punto final
+punto_final_seleccionado = False
+punto_final = None
+
+#Función para dibujar la matriz en la ventana
+def dibujar_matriz():
+    for fila in range(FILA):
+        for columna in range(COLUMNA):
+            # Calcular las coordenadas de la celda
+            x = columna * ANCHO_CELDA
+            y = fila * ALTO_CELDA
+
+            # Dibujar el fondo de la celda
+            if matriz[fila][columna] == 0:
+                color = BLANCO
+            else:
+                color = GRIS
+            pygame.draw.rect(ventana, color, (x, y, ANCHO_CELDA, ALTO_CELDA))
+
+            # Dibujar el contorno de la celda
+            pygame.draw.rect(ventana, NEGRO, (x, y, ANCHO_CELDA, ALTO_CELDA), 1)
+
+            # Dibujar la selección verde si el punto inicial está seleccionado
+            if punto_inicial_seleccionado and punto_inicial == (fila, columna):
+                pygame.draw.rect(ventana, VERDE, (x, y, ANCHO_CELDA, ALTO_CELDA), 4)
+
+            # Dibujar la selección roja si el punto final está seleccionado
+            if punto_final_seleccionado and punto_final == (fila, columna):
+                pygame.draw.rect(ventana, ROJO, (x, y, ANCHO_CELDA, ALTO_CELDA), 4)
+
+
+# Función para definir el punto inicial mediante clic de mouse
+def puntoInicial():
+    global punto_inicial_seleccionado, punto_inicial
+    punto_inicial_seleccionado = True
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif evento.type == pygame.MOUSEBUTTONDOWN and punto_inicial_seleccionado:
+                x, y = pygame.mouse.get_pos()
+                fila = y // ALTO_CELDA
+                columna = x // ANCHO_CELDA
+                punto_inicial = (fila, columna)
+                print(f"Punto Inicial: {punto_inicial}")
+                return punto_inicial
+
+# Función para definir el punto final mediante clic de mouse
+def puntoFinal():
+    global punto_final_seleccionado, punto_final
+    punto_final_seleccionado = True
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif evento.type == pygame.MOUSEBUTTONDOWN and punto_final_seleccionado:
+                x, y = pygame.mouse.get_pos()
+                fila = y // ALTO_CELDA
+                columna = x // ANCHO_CELDA
+                punto_final = (fila, columna)
+                print(f"Punto Final: {punto_final}")
+                return punto_final
+
+# Bucle principal
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    # Limpiar la pantalla
+    ventana.fill(NEGRO)
+
+    # Dibujar la matriz en la ventana
+    dibujar_matriz()
+
+    # Actualizar la pantalla
+    pygame.display.flip()
+
+    # Llamar a la función puntoInicial
+    puntoInicial()
+
+    #Llama a la funcion puntoFinal
+    puntoFinal()
