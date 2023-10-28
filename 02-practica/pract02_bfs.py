069245db3ca8825421841c9e4bf6f4462e97c2a7
Elizabeth Mtz <lo.eliza@hotmail.com>
2023-10-28 12:58:04 -0600

    feat: bfs

--- 02-practica/pract02_bfs.py
+++ 02-practica/pract02_bfs.py
@@ -10,17 +10,17 @@ NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
 GRIS = (128, 128, 128)
 VERDE = (0, 255, 0)
-ROJO= (255, 0, 0)
-AZUL=(0,0,255)
-# Tamaño de la ventana y de la matriz
+ROJO = (255, 0, 0)
+AZUL = (0, 0, 255)
+
+# Tamaños y configuración
 ANCHO = 600
 ALTO = 600
 FILA = 15
 COLUMNA = 15
-
-# Tamaño de cada celda
 ANCHO_CELDA = ANCHO // COLUMNA
 ALTO_CELDA = ALTO // FILA
+PERSONAJE = 4
 
 # Definir la nueva matriz
 matriz = [
@@ -59,7 +59,7 @@ matriz_aux = [
     [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1]
 ]
-# Inicializar Pygame
+# Inicializar Pygame ventana
 ventana = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("Practica 2")
 
@@ -141,26 +141,7 @@ def puntoFinal():
                     print(f"Punto Final: {punto_final}")
                     return punto_final
 
-# Función para manejar el movimiento del personaje
-def mover_personaje(tecla):
-    global posicion_personaje, pasos  # Agrega `pasos` aquí para que puedas modificarla
 
-    fila, columna = posicion_personaje
-    tecla = pygame.key.get_pressed()
-
-    # Guardar las posiciones adyacentes antes de mover al personaje
-    restaurar_casillas_adyacentes(fila, columna)
-
-    nueva_fila, nueva_columna = fila, columna
-
-    if tecla[pygame.K_w] and fila > 0 and matriz_aux[fila - 1][columna] == 0:
-        nueva_fila -= 1
-    elif tecla[pygame.K_s] and fila < FILA - 1 and matriz_aux[fila + 1][columna] == 0:
-        nueva_fila += 1
-    elif tecla[pygame.K_a] and columna > 0 and matriz_aux[fila][columna - 1] == 0:
-        nueva_columna -= 1
-    elif tecla[pygame.K_d] and columna < COLUMNA - 1 and matriz_aux[fila][columna + 1] == 0:
-        nueva_columna += 1
 
     # Verificar si la nueva posición es válida en la matriz original
     if 0 <= nueva_fila < FILA and 0 <= nueva_columna < COLUMNA and matriz[nueva_fila][nueva_columna] == 0:
@@ -172,19 +153,24 @@ def mover_personaje(tecla):
         matriz_aux[fila][columna] = PERSONAJE
         posicion_personaje = (fila, columna)
 
+        descubrir_casillas_adyacentes(fila, columna) # <-- Esta línea está correctamente colocada aquí
+
         pasos = contarPasos(pasos)  # Llamada a contarPasos cada vez que el personaje se mueve
 
-        # Restaurar las casillas adyacentes en la matriz original después del movimiento
-        restaurar_casillas_adyacentes(fila, columna)
 
-#Function that hides the matrix except the character
+
 def ocultaMatriz():
     for fila in range(FILA):
         for columna in range(COLUMNA):
-            if matriz[fila][columna] != PERSONAJE:
+            if (fila, columna) != posicion_personaje:
                 matriz[fila][columna] = 2
 
-#Restaura las casillas conforme el personaje se mueve
+def descubrir_casillas_adyacentes(fila, columna):
+    for dx, dy in [(0, -1), (-1, 0), (0, 1), (1, 0)]:
+        nueva_fila, nueva_columna = fila + dx, columna + dy
+        if 0 <= nueva_fila < FILA and 0 <= nueva_columna < COLUMNA:
+            matriz[nueva_fila][nueva_columna] = matriz_aux[nueva_fila][nueva_columna]
+
 def restaurar_casillas_adyacentes(fila, columna):
     for i in range(max(0, fila - 1), min(FILA, fila + 2)):
         for j in range(max(0, columna - 1), min(COLUMNA, columna + 2)):
@@ -195,72 +181,109 @@ def contarPasos(pasos):
     pasos += 1
     return pasos
 
-#Clase nodo para el algoritmo BFS
-'''class nodo:
-    def __init__(self, valor, fila, columna):
-        self.valor = valor
+# Clase nodo para el algoritmo BFS
+class Nodo:
+    def __init__(self, fila, columna, padre=None):
         self.fila = fila
         self.columna = columna
+        self.padre = padre
 
-# Función para encontrar la ruta utilizando BFS
-def encontrar_ruta_bfs(matriz, inicio, destino):
+def imprimir_arbol_bfs(nodo):
+    if nodo is None:
+        return
+    print(f"({nodo.fila}, {nodo.columna})")
+    for dx, dy in [(0, -1), (-1, 0), (0, 1), (1, 0)]:
+        nueva_fila, nueva_columna = nodo.fila + dx, nodo.columna + dy
+        if 0 <= nueva_fila < FILA and 0 <= nueva_columna < COLUMNA and matriz[nueva_fila][nueva_columna] == 0:
+            imprimir_arbol_bfs(Nodo(nueva_fila, nueva_columna, nodo))
+
+# Función BFS para encontrar la ruta más corta
+
+def bfs(inicio, destino):
+    fila_inicial, columna_inicial = inicio
+    fila_destino, columna_destino = destino
+
+    nodo_inicial = Nodo(fila_inicial, columna_inicial)
     visitado = set()
-    fila, columna = inicio
-    cola = deque([(fila, columna, [])])
+    cola = deque([nodo_inicial])
 
     while cola:
-        fila, columna, ruta = cola.popleft()
-        if (fila, columna) == destino:
-            return ruta + [(fila, columna)]
+        nodo_actual = cola.popleft()
+
+        if (nodo_actual.fila, nodo_actual.columna) == (fila_destino, columna_destino):
+            path = []
+            while nodo_actual:
+                path.append((nodo_actual.fila, nodo_actual.columna))
+                nodo_actual = nodo_actual.padre
+            return path[::-1]
 
         for dx, dy in [(0, -1), (-1, 0), (0, 1), (1, 0)]:
-            nueva_fila, nueva_columna = fila + dx, columna + dy
-            if (
-                0 <= nueva_fila < FILA
-                and 0 <= nueva_columna < COLUMNA
-                and matriz[nueva_fila][nueva_columna] == 0
-                and (nueva_fila, nueva_columna) not in visitado
-            ):
-                cola.append((nueva_fila, nueva_columna, ruta + [(fila, columna)]))
+            nueva_fila, nueva_columna = nodo_actual.fila + dx, nodo_actual.columna + dy
+            if (0 <= nueva_fila < FILA and 0 <= nueva_columna < COLUMNA and
+               matriz_aux[nueva_fila][nueva_columna] == 0 and  # Usar matriz_aux aquí
+               (nueva_fila, nueva_columna) not in visitado):
                 visitado.add((nueva_fila, nueva_columna))
+                nodo_vecino = Nodo(nueva_fila, nueva_columna, nodo_actual)
+                cola.append(nodo_vecino)
+
     return None
 
-# Encontrar la ruta desde el punto inicial hasta el punto final
-ruta = encontrar_ruta_bfs(matriz, punto_inicial, punto_final)
+dibujar_matriz()
+pygame.display.flip()
 
-if ruta is not None:
-    # Iterar a través de la ruta y mover al personaje automáticamente
-    for fila, columna in ruta:
-        matriz[fila][columna] = PERSONAJE
-        matriz[posicion_personaje[0]][posicion_personaje[1]] = 0
-        posicion_personaje = (fila, columna)
-        dibujar_matriz()
-        pygame.display.flip()
-        pygame.time.delay(200)  # Agregar un retraso para ver el movimiento'''
+# Muestra la matriz y espera a que el usuario seleccione el punto inicial
+punto_inicial = puntoInicial()
 
-# Bucle principal
-cont = 1
-destino = 0
+# Muestra nuevamente la matriz y espera a que el usuario seleccione el punto final
+punto_final = puntoFinal()
+
+# Una vez que ambos puntos han sido seleccionados, oculta la matriz
+ocultaMatriz()
+
+
+ruta = bfs(punto_inicial, punto_final)
+if ruta is None:
+    print("No hay una ruta válida entre el punto inicial y el punto final.")
+    sys.exit()
+
+print("Movimientos realizados:")
+contador_movimientos = 0
+for i in range(1, len(ruta)):
+    fila_actual, columna_actual = ruta[i]
+    fila_anterior, columna_anterior = ruta[i-1]
+    
+    if fila_actual == fila_anterior - 1:
+        print("Arriba")
+        contador_movimientos += 1
+    elif fila_actual == fila_anterior + 1:
+        print("Abajo")
+        contador_movimientos += 1
+    elif columna_actual == columna_anterior - 1:
+        print("Izquierda")
+        contador_movimientos += 1
+    elif columna_actual == columna_anterior + 1:
+        print("Derecha")
+        contador_movimientos += 1
+
+# Imprime el total de movimientos
+print(f"Total de movimientos realizados: {contador_movimientos}")
+
+indice_ruta = 1  # Comenzamos en 1 ya que el punto inicial ya está en la matriz
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif evento.type == pygame.KEYDOWN:
-            if punto_inicial_seleccionado and destino == 0:
-                mover_personaje(evento.key)
-                # Verificar si se llegó al destino
-                if posicion_personaje == punto_final:
-                    print("Llegaste a tu destino!")
-                    print(f"Pasos dados: {pasos}")
-                    destino = 1
+
+    if indice_ruta < len(ruta):
+        fila, columna = ruta[indice_ruta]
+        matriz[posicion_personaje[0]][posicion_personaje[1]] = 0
+        matriz[fila][columna] = PERSONAJE
+        posicion_personaje = (fila, columna)
+        indice_ruta += 1
+        pygame.time.delay(200)  # Agregar un retraso para ver el movimiento
+
     # Dibujar la matriz en la ventana
     dibujar_matriz()
     # Actualizar la pantalla
     pygame.display.flip()
-
-    if cont:
-        puntoInicial()
-        puntoFinal()
-        ocultaMatriz()
-        cont = cont - 1
