02d74bc2cc2a42eddf47e5d92186c207c06fbf32
Eliot Valdes <eliottvaldes@hotmail.com>
2024-01-08 01:34:06 -0600

    implementacion inicial de laberinto a estrella: - dibuja laberinto - muestra personajes - selecciona punto de partida - selecciona punto de llegada - pinta puntos en puntos de partida y punto de llegada

---
+++ 03-a_estrella/laberinto_a*.py
@@ -0,0 +1,245 @@
+"""
+Respira profundo, lee lentamente y piensa paso a paso.
+Usando python debes hacer lo siguiente:
+1.- crear una interfaz grafica en la que se muestre un laberinto dado una matriz de 1 y 0.
+2.- el sistema debe permitirle al usuario seleccionar uno de los 4 personajes (humano, mono, pulpo, pie grande)
+3.- el usuario puede seleccionar el punto de partida de su personaje dentro del laberinto.
+3.1 El sistema debe validar que el punto de partida seleccionado no sea un punto no permitido por el personaje.
+4.- el usuario puede seleccionar el punto de llegada de su personaje dentro del laberinto.
+4.1 El sistema debe validar que el punto de llegada seleccionado no sea un punto no permitido por el personaje.
+4.2.- el sistema debe de colocar un puntito rojo en el punto de partida y un puntito verde en el punto de llegada.
+5.- el sistema debe de encontrar la ruta más indicada usando el argoritmo A estrella. La funcion debe devolver los nodos abiertos, los nodos cerrados, el arbol generado y la ruta ideal.
+6.- el sistema debe mover el personaje por el laberinto segun la ruta ideal del algoritmo A estrella.
+7.- El sistema debe de colocar en el laberinto la notación indicada en los pasos siguientes:
+7.1- El sistema debe colocar una letra 'O' en laberinto segun sean los nodos abiertos del algoritmo a estrella.
+7.2.- El sistema debe colocar una letra 'X' en laberinto segun sean los nodos cerrados del algoritmo a estrella.
+7.3- El sistema debe colocar entre parentesis el costo total del movimiento en cada una de las celdas visitadas por el algoritmo A estrella. Ejemplo de un nodo abierto con costo de 5: O(5). Ejemplo de un nodo cerrado con costo de 2: X(2).
+8.- El sistema debe de imprimir en terminal el arbol generado con la solución del algoritmo a estrella. El formato de impresión del arbol debe ser como un directorio de carpetas, es decir, por niveles y saltos de linea.
+
+NOTAS:
++ el sistema debe de tomar en cuenta los pesos del laberinto ya que cambian de  a cuerdo al personaje seleccionado
++ el sistema debe de considerar que la matriz está creada usando los colores definidos en colorDictionary
++ el sistema debe detectar si un personaje no puede pasar por una celda que se identifica en cada personaje con el valor de 'fieldsNotAllowed'
+
+---
+Toma en cuenta el codigo ya existente:
+
+"""
+
+import pygame
+import sys
+
+# Constantes
+WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
+CELL_SIZE = 40  # Asumiendo que cada celda del laberinto es un cuadrado de 40x40
+
+# Colores
+colorDictionary = {
+    "0": (128, 128, 128),  # Gris
+    "1": (255, 203, 164),  # Tierra
+    "2": (10, 191, 178),   # Agua
+    "3": (255, 175, 7),    # Arena
+    "4": (17, 243, 64),    # Bosque
+    "5": (128, 0, 128),    # Pantano
+    "6": (255, 255, 255),  # Nieve
+}
+
+# Matriz del laberinto
+mazeMatrix = [["4","4","4","1","1","1","1","1","1","1","1","1","1","4","4"],["4","4","4","4","4","4","1","1","1","1","4","3","4","4","4"],["4","4","4","2","2","4","1","4","1","1","3","3","4","1","1"],["4","4","2","2","2","2","1","4","1","1","3","3","1","1","1"],["4","4","2","2","2","2","1","4","4","1","1","3","1","1","1"],["4","4","4","2","2","1","1","1","1","1","1","3","3","3","3"],["4","4","4","0","0","0","1","1","1","1","1","3","3","3","3"],["4","4","4","1","0","4","4","1","1","3","3","3","3","2","2"],["4","4","4","1","1","4","4","0","1","3","3","3","2","2","2"],["4","4","1","1","1","1","0","0","0","3","3","2","2","2","2"],["4","4","1","1","1","1","1","1","3","3","3","2","2","2","2"],["4","4","1","1","1","1","1","1","3","3","2","2","2","2","2"],["4","4","1","4","4","1","1","3","3","3","2","2","2","2","2"],["4","4","4","4","4","4","1","3","3","3","2","2","2","2","2"],["4","4","4","4","4","4","1","3","3","3","2","2","2","2","2"]]
+
+characters = {
+    'person': {
+        'id': 1,
+        'name': 'Humano',
+        'icon': '👨',
+        'fieldsNotAllowed': ['0'],  # negro - montaña
+        'heights': {
+            # color_index: peso_de_movi
+            0: 0,
+            1: 1,
+            2: 2,
+            3: 3,
+            4: 4,
+            5: 5,
+            6: 6,
+        }
+    },
+    'monkey': {
+        'id': 2,
+        'name': 'Mono',
+        'icon': '🐒',
+        'fieldsNotAllowed': ['0', '6'],  # negro - montaña
+        'heights': {
+            # color_index: peso_de_movi
+            0: 0,
+            1: 2,
+            2: 4,
+            3: 3,
+            4: 1,
+            5: 5,
+            6: 0,
+        }
+    },
+    'octopus': {
+        'id': 3,
+        'name': 'Pulpo',
+        'icon': '🐙',
+        'fieldsNotAllowed': ['0', '3', '6'],
+        'heights': {
+            # color_index: peso_de_movi
+            0: 0,
+            1: 2,
+            2: 1,
+            3: 0,
+            4: 3,
+            5: 2,
+            6: 0,
+        }
+    },
+    'sasquatch': {
+        'id': 4,
+        'name': 'Pie grande',
+        'icon': '🦶',
+        'fieldsNotAllowed': ['2', '3'],
+        'heights': {
+            # color_index: peso_de_movi
+            0: 15,
+            1: 4,
+            2: 0,
+            3: 0,
+            4: 4,
+            5: 5,
+            6: 3,
+        }
+    }    
+}
+
+# Inicialización de Pygame
+pygame.init()
+# Define una fuente para el texto
+font_size = 20
+font = pygame.font.Font(None, font_size)
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Laberinto A*")
+
+
+def main():
+    running = True
+    
+    character = select_character()  # Selección del personaje     
+    start_point = select_point("Seleccione el punto de partida", character, validate_point)
+    end_point = select_point("Seleccione el punto de llegada", character, validate_point)
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))
+        draw_maze()
+        draw_start_end_points(start_point, end_point)  # Dibujar puntos de inicio y fin
+        pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
+
+# funcion para dibujar el laberinto
+def draw_maze():
+    for y, row in enumerate(mazeMatrix):
+        for x, cell in enumerate(row):
+            rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, colorDictionary[cell], rect)
+            pygame.draw.rect(screen, (0, 0, 0), rect, 1)
+
+# funcion para crear los botones de seleccion de personaje
+def create_character_buttons():
+    button_width, button_height = 100, 50
+    buttons = {}
+    x = 20  # Posición inicial x para el primer botón
+    y = 20  # Posición inicial y para los botones
+
+    for character_key, character_info in characters.items():
+        buttons[character_key] = pygame.Rect(x, y, button_width, button_height)
+        x += button_width + 25  # Ajusta el espacio entre los botones
+
+    return buttons
+
+# funcion para seleccionar el personaje usando los botones
+def select_character():
+    character_button_rect = create_character_buttons()
+    selected_character = None
+    while selected_character is None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                for character_key, rect in character_button_rect.items():
+                    if rect.collidepoint(mouse_pos):
+                        selected_character = character_key
+                        break
+
+        screen.fill((200, 225, 255))  # Fondo para la selección de personajes
+
+        # Dibuja los botones y el nombre de los personajes
+        for character_key, rect in character_button_rect.items():
+            pygame.draw.rect(screen, (0, 0, 255), rect)  # Dibuja rectángulos como botones
+
+            # Renderiza el nombre del personaje en el botón
+            text_surface = font.render(characters[character_key]['name'], True, (0, 0, 0))
+            text_rect = text_surface.get_rect(center=rect.center)
+            screen.blit(text_surface, text_rect)
+
+        pygame.display.flip()
+
+    return selected_character
+
+
+def select_point(message, character, validation_function):    
+    print(message)
+    point = None
+    while point is None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = event.pos
+                cell_x, cell_y = mouse_x // CELL_SIZE, mouse_y // CELL_SIZE
+                if 0 <= cell_x < len(mazeMatrix[0]) and 0 <= cell_y < len(mazeMatrix):
+                    point = (cell_x, cell_y)
+                    if not validation_function(character, point):
+                        print("Punto no válido, seleccione otro.")
+                        point = None  # Reiniciar la selección de punto
+                    else:
+                        print(f"Punto seleccionado: {point}")
+
+        screen.fill((0, 0, 0))  # Fondo negro
+        draw_maze()
+        pygame.display.flip()
+
+    return point
+
+
+# funcion para validar que los puntos seleccionados son validos
+def validate_point(character, selected_point):    
+    cell_value = mazeMatrix[selected_point[1]][selected_point[0]]
+    if cell_value in characters[character]['fieldsNotAllowed']:
+        return False
+    return True 
+    
+
+# funcion para dibujar los puntos de partida y puntos de llegada
+def draw_start_end_points(start_point, end_point):    
+    start_center = (start_point[0] * CELL_SIZE + CELL_SIZE // 2, start_point[1] * CELL_SIZE + CELL_SIZE // 2)
+    pygame.draw.circle(screen, (0, 0, 0), start_center, CELL_SIZE // 2)  # Círculo negro para el inicio
+
+    end_center = (end_point[0] * CELL_SIZE + CELL_SIZE // 2, end_point[1] * CELL_SIZE + CELL_SIZE // 2)
+    pygame.draw.circle(screen, (0, 0, 255), end_center, CELL_SIZE // 2)  # Círculo azul para el fin
+
+
+
+if __name__ == "__main__":
+    main()
